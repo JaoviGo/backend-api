bcdf4aeee1272cc7760f335dbf4485e460ddf8fe
JaoviGo <bcjcpunk3@gmail.com>
2026-04-17 09:51:32 -0500

    fix chat endpoint

--- server.js
+++ server.js
@@ -7,25 +7,19 @@ app.use(express.json());
 
 const PORT = process.env.PORT || 3000;
 
-// TEST
 app.get('/', (req, res) => {
   res.send("Backend activo");
 });
 
-// CHAT
 app.post('/chat', (req, res) => {
   const { message } = req.body;
-
-  console.log("Mensaje recibido:", message);
 
   res.json({
     reply: "JaoviGo activo: " + message
   });
 });
 
-// LEAD
 app.post('/lead', (req, res) => {
-  console.log("Nuevo lead:", req.body);
   res.json({ status: "ok" });
 });
 
